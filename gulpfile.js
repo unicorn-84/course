1a74997e35460094a03d80bafa20258b06060955
unicorn-84 <dev.kapitonov@gmail.com>
2017-08-30 01:16:31 +0300

    Added base gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,64 @@
 const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const del = require('del');
+const gulpIf = require('gulp-if');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+
+const isDevelopment = false;
+
+gulp.task('clean', (callback) => {
+  del('./build');
+  callback();
+});
+
+gulp.task('html', () =>
+  gulp.src('src/*.html', { since: gulp.lastRun('html') })
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true,
+    }))
+    .pipe(gulp.dest('./build')),
+);
+
+gulp.task('styles', () =>
+  gulp.src('./src/styles/main.scss')
+    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false,
+    }))
+    .pipe(cleanCSS({
+      level: {
+        1: {
+          specialComments: 0,
+        },
+      },
+    }))
+    .pipe(gulpIf(isDevelopment, sourcemaps.write()))
+    .pipe(gulp.dest('./build/styles')),
+);
+
+gulp.task('scripts', () =>
+  gulp.src('./src/scripts/*.js')
+    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+    .pipe(babel({
+      presets: [
+        ['env', {
+          targets: {
+            browsers: ['last 2 versions'],
+          },
+        }],
+        ['es2015'],
+      ],
+    }))
+    .pipe(uglify())
+    .pipe(gulpIf(isDevelopment, sourcemaps.write()))
+    .pipe(gulp.dest('./build/scripts')),
+);
+gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'scripts', 'html')));
